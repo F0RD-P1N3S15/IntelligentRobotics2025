10768fef996ad9db5f3d2bafe007d05dd77b76a6
F0RD-P1N3S15 <bridget.parish@student.ehb.be>
2025-10-21 09:49:07 +0200

    Add previous code

---
+++ PythonProject/Test.py
@@ -0,0 +1,13 @@
+# Vraag 2 afstanden en bereken totale route
+afstand1 = float(input("Eerste afstand (in meter): "))
+afstand2 = float(input("Tweede afstand (in meter): "))
+afstand_totaal = afstand1 + afstand2
+
+print("Totale afstand: ", afstand_totaal, "m")
+# OF
+print("Totale afstand: ", afstand1 + afstand2, "m")
+
+
+# Vraag tijd in seconden en toon als minuten:seconden
+# Bereken snelheid uit RPM en wielomtrek
+# Vraag robotnaam en snelheid, print zin met resultaat
